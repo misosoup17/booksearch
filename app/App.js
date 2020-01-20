646bbde3cde3e8e676e68f63a0f542cd862239d3
misosoup17 <misagaspar@icloud.com>
2020-01-20 12:35:56 -0800

    App.js

---
+++ app/App.js
@@ -0,0 +1,7 @@
+var React = require('react');
+var ReactDom = require('react-dom');
+
+//Main Component
+var Main = require('./Components/Main');
+
+ReactDOM.render(<Main />, document.getElementById('app'));
---
+++ app/Components/Main.js
@@ -0,0 +1,11 @@
+var axios = require('axios');
+
+//Include React
+var React = require('react');
+
+
+//Helper Functions
+
+var helpers = require('./utils/helpers.js');
+
+//Main Component
