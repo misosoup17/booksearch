1c9dbd7d342026d6d3388e8293683d37317452fb
misosoup17 <misagaspar@icloud.com>
2020-01-20 16:48:22 -0800

    updated

---
+++ client/src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Root from "./root";
+import configureStore from "./store";
+import * as serviceWorker from "./serviceWorker";
+
+ReactDOM.render(
+  <Root store={configureStore()} />,
+  document.getElementById("root")
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
