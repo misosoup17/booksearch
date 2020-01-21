ba27030a65728cb38f2953c1c15ddfd65272d3f5
misosoup17 <misagaspar@icloud.com>
2020-01-20 19:12:53 -0800

    updated

---
+++ client/build/precache-manifest.2b6473781da6718493bedbd0b2b9c579.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "510b41711fae63d9b9d6",
+    "url": "/static/css/main.69d45277.chunk.css"
+  },
+  {
+    "revision": "510b41711fae63d9b9d6",
+    "url": "/static/js/main.29fe9780.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8225d3981132153b160e",
+    "url": "/static/js/2.f57d1115.chunk.js"
+  },
+  {
+    "revision": "cfc87b7d3c696d03a78b8b42907f64d2",
+    "url": "/index.html"
+  }
+];
